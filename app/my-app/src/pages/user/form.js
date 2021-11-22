47a4d8fb6cfc6cff375b8a06868d7fe7e1197afb
Dayan Freitas <dayan.freitas@catolicasc.org.br>
2021-11-21 23:57:59 -0300

    form criação de usuário

---
+++ app/my-app/src/pages/user/form.js
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from 'react';
+import { useHistory } from "react-router-dom";
+
+import userService from '../../services/user';
+import StarIcon from '@material-ui/icons/Star';
+import { FormControl, InputLabel, Input, Box, Typography, Grid, Button, Card, CardHeader, CardContent, Avatar, Container } from "@material-ui/core";
+
+function Player() {
+    const history = useHistory();
+    const [email, setEmail] = useState('')
+    const [name, setName] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    // const [differentPasswords, setDifferentPasswords] = useState('')
+    
+
+    useEffect(() => {
+        // userService.get
+    }, [])
+
+    const canSave = () => {
+        // setDifferentPasswords('')
+        if (!email || !name || !password || !confirmPassword) {
+            return true
+        }
+
+        if (password != confirmPassword) {
+            // setDifferentPasswords('different passwords')
+            return true
+        }
+        
+        return false
+    }
+
+    const save = () => {
+        userService.create({
+            name, email, password, access_type_id: 3
+        }).then((r) => {
+            if (r.status == 201) {
+                history.push('/')
+            }
+        })
+    }
+
+    return (
+        <Container>
+            <Typography variant="h3">
+                Formulário de User
+            </Typography>
+
+
+            <form>
+                <Box sx={{
+                    display: 'grid',
+                    gap: 10,
+                    gridTemplateColumns: 'repeat(2, 1fr)',
+                }}>
+
+                    <FormControl required>
+                        <InputLabel htmlFor="email">Email</InputLabel>
+                        <Input id="email" value={email} onChange={(e) => { setEmail(e.target.value) }} placeholder="email@gmail.com" required/>
+                    </FormControl>
+
+                    <FormControl required>
+                        <InputLabel htmlFor="name">Name</InputLabel>
+                        <Input id="name" value={name} onChange={(e) => { setName(e.target.value) }} placeholder="Name"/>
+                    </FormControl>
+
+                    <FormControl required>
+                        <InputLabel htmlFor="password">Senha</InputLabel>
+                        <Input type="password" id="password" value={password} onChange={(e) => { setPassword(e.target.value) }}  placeholder="***"/>
+                    </FormControl>
+
+                    <FormControl required>
+                        <InputLabel htmlFor="confirmPassword">Confirmação de senha</InputLabel>
+                        <Input type="password" id="confirmPassword" value={confirmPassword} onChange={(e) => { setConfirmPassword(e.target.value) }} placeholder="***"/>
+                    </FormControl>
+                </Box>
+
+                <Box sx={{
+                    display: 'grid',
+                    gap: 10,
+                    marginTop:10,
+                    gridTemplateColumns: 'repeat(1, 1fr)',
+                }}>
+                    {/* <div>{
+                        differentPasswords ? differentPasswords : ''
+                    }</div> */}
+
+                    <Button color="primary" variant="outlined" disabled={canSave()} onClick={() => {save()}}>Criar</Button>
+                    <Button color="secondary" variant="outlined" onClick={() => { history.push('/') }}>Voltar</Button>
+                </Box >
+            </form>
+
+        </Container>
+    );
+}
+
+export default Player;
--- app/my-app/src/router.js
+++ app/my-app/src/router.js
@@ -4,6 +4,8 @@ import Game from './pages/games'
 import Login from './pages/login'
 import Player from './pages/players'
 
+import UserForm from './pages/user/form'
+
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 
 const isAuthenticated = () => {
@@ -37,6 +39,9 @@ const Routes =  (props) => {
                 <Route component={Login} path="/login" exact/>
                 <PrivateRouter component={Game} path="/games" exact/>
                 <PrivateRouter component={Player} nav={props.nav} path="/players" exact/>
+                {/* <PrivateRouter component={PlayerForm} nav={props.nav} path="/player/create" exact/> */}
+                <PrivateRouter component={UserForm} nav={props.nav} path="/user/create" exact/>
+                
                 {/* <Route path='*' component={Game} /> */}
             </Switch>
         </BrowserRouter>
