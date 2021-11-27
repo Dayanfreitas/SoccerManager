b2a46bc0636ba1af5f1396365f487fc598b2d03a
Dayan Freitas <dayan.freitas@catolicasc.org.br>
2021-11-26 23:21:20 -0300

    formik

--- app/my-app/src/pages/user/form.js
+++ app/my-app/src/pages/user/form.js
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import { useFormik } from 'formik';
+import * as Yup from "yup";
 
 import userService from '../../services/user';
-// import StarIcon from '@material-ui/icons/Star';
 import {
   FormControl,
   InputLabel,
@@ -11,37 +12,48 @@ import {
   Typography,
   Button,
   Container,
+  FormHelperText
 } from '@material-ui/core';
 
 function Player() {
   const history = useHistory();
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  
+  useEffect(() => {
+  }, []);
 
-  useEffect(() => {}, []);
-
-  const canSave = () => {
-    if (!email || !name || !password || !confirmPassword) {
-      return true;
-    }
-
-    if (password !== confirmPassword) {
-      return true;
-    }
-
-    return false;
-  };
-
-  const save = () => {
-    userService
-      .create({
+  const validationSchema = Yup.object({
+    name: Yup.string().required("O nome é obrigatório"),
+    email: Yup.string().email("Insira um e-mail válido").required("O email é obrigatório"),
+    password: Yup.string().min(6, "No mínimo 6 caracteres").required("A senha é obrigatória"),
+    password_confirm: Yup.string().oneOf([Yup.ref("password"), null], "A senha não corresponde").required("Confirmação de senha é obrigatória"),
+  });
+  
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      password: '',
+      password_confirm: '',
+    },
+    validationSchema,
+    enableReinitialize: true,
+    onSubmit: (values) => {
+      const {
         name,
         email,
-        password,
-        access_type_id: 3,
-      })
+        password
+      }  = values
+      save({name, email, password});
+    }
+  });
+
+
+  const save = (params) => {
+    //TODO: PASSA PARA O BACK 
+    params.access_type_id = 3
+
+    userService
+      .create(params)
       .then((r) => {
         if (r.status === 201) {
           history.push('/players');
@@ -49,11 +61,26 @@ function Player() {
       });
   };
 
+  const FormikFormHelperText = (props) => {
+    return (
+      <div>
+        {
+          formik.touched[props.name] && formik.errors[props.name] ? 
+          <FormHelperText>{formik.errors[props.name]}</FormHelperText> :
+          ''
+        }
+      </div>
+    )
+  }
+
   return (
     <Container>
-      <Typography variant="h3">Formulário de User</Typography>
-
-      <form>
+      <Typography variant="h4">Formulário de User</Typography>
+      <form
+        onSubmit={formik.handleSubmit} 
+        className={!formik.isValid ? 'not-valid' : ''}
+        autoComplete="off"
+      >
         <Box
           sx={{
             display: 'grid',
@@ -61,57 +88,62 @@ function Player() {
             gridTemplateColumns: 'repeat(2, 1fr)',
           }}
         >
-          <FormControl required>
+          <FormControl required error={formik.touched.email && Boolean(formik.errors.email)}>
             <InputLabel htmlFor="email">Email</InputLabel>
             <Input
               id="email"
-              value={email}
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
+              name="email"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.email}
               placeholder="email@gmail.com"
-              required
             />
+            <FormikFormHelperText name="email"></FormikFormHelperText>
           </FormControl>
 
-          <FormControl required>
+          <FormControl required error={formik.touched.name && Boolean(formik.errors.name)}>
             <InputLabel htmlFor="name">Name</InputLabel>
             <Input
               id="name"
-              value={name}
-              onChange={(e) => {
-                setName(e.target.value);
-              }}
+              name="name"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.name}
               placeholder="Name"
             />
+            <FormikFormHelperText name="name"></FormikFormHelperText>
           </FormControl>
 
-          <FormControl required>
+          <FormControl required error={formik.touched.password && Boolean(formik.errors.password)}>
             <InputLabel htmlFor="password">Senha</InputLabel>
             <Input
               type="password"
               id="password"
-              value={password}
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
+              name="password"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.password}
+              error={formik.touched.password && Boolean(formik.errors.password)}
               placeholder="***"
             />
+            <FormikFormHelperText name="password"></FormikFormHelperText>
           </FormControl>
 
-          <FormControl required>
-            <InputLabel htmlFor="confirmPassword">
+          <FormControl required error={formik.touched.password_confirm && Boolean(formik.errors.password_confirm)}>
+            <InputLabel htmlFor="password_confirm">
               Confirmação de senha
             </InputLabel>
             <Input
               type="password"
-              id="confirmPassword"
-              value={confirmPassword}
-              onChange={(e) => {
-                setConfirmPassword(e.target.value);
-              }}
+              id="password_confirm"
+              name="password_confirm"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.password_confirm}
+              error={formik.touched.password_confirm && Boolean(formik.errors.password_confirm)}
               placeholder="***"
             />
+            <FormikFormHelperText name="password_confirm"></FormikFormHelperText>
           </FormControl>
         </Box>
 
@@ -124,16 +156,15 @@ function Player() {
           }}
         >
           <Button
+            type="submit"
             color="primary"
             variant="outlined"
-            disabled={canSave()}
-            onClick={() => {
-              save();
-            }}
+            disabled={!formik.isValid}
           >
             Criar
           </Button>
           <Button
+            type="button"
             color="secondary"
             variant="outlined"
             onClick={() => {
